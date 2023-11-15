9dda26b551a7f32969dd9bb2a5d8d067da12bea4
huy <nhocbehue270@gmail.com>
2023-11-15 09:54:39 +0700

    add 2 user entity in system that is don't need account to access feature of system

--- src/main/java/com/magicpost/app/magicPost/user/Customer.java
+++ src/main/java/com/magicpost/app/magicPost/user/Customer.java
@@ -1,8 +1,27 @@
 package com.magicpost.app.magicPost.user;
 
 import com.magicpost.app.magicPost.address.Address;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public class Customer extends User{
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String phone;
+    @NotNull
     private Address address;
+    @NotNull
     public Integer zipcode;
 }
--- src/main/java/com/magicpost/app/magicPost/user/Shipper.java
+++ src/main/java/com/magicpost/app/magicPost/user/Shipper.java
@@ -1,4 +1,25 @@
 package com.magicpost.app.magicPost.user;
 
-public class Shipper extends User{
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Shipper{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String name;
+    private String email;
+    private String phone;
+    private Boolean isActive = true;
 }
+
