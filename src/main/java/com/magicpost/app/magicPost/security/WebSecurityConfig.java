566f1c761511cd9330b4f515279f536d583c694a
huy <nhocbehue270@gmail.com>
2023-12-31 21:46:38 +0700

    ignore security for some endpoint

--- src/main/java/com/magicpost/app/magicPost/security/WebSecurityConfig.java
+++ src/main/java/com/magicpost/app/magicPost/security/WebSecurityConfig.java
@@ -14,11 +14,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -106,17 +105,22 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return webSecurity -> webSecurity.ignoring().requestMatchers("/api/v1/auth/**", "/error", "/swagger-ui/**",
+                "api/v1/express-orders/**");
+    }
+
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(configure -> configure.jwt(Customizer.withDefaults()))
                 .authorizeHttpRequests(
                         authorization -> authorization
-                                .requestMatchers("/api/v1/auth/**", "/error", "/swagger-ui/**",
-                                        "api/v1/express-orders/**").permitAll()
                                 .anyRequest().authenticated()
                 )
+                .oauth2ResourceServer(configure -> configure.jwt(Customizer.withDefaults()))
                 .exceptionHandling(
                         exception -> exception
                                 .authenticationEntryPoint(authenticationEntryPoint)
